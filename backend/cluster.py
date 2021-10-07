6790c9f3318530127a0d6468feba9ba3539e71bd
Edward Chang <echang@unb.ca>
2021-10-07 05:03:18 -0300

    Add comments for clarity

--- backend/cluster.py
+++ backend/cluster.py
@@ -9,6 +9,15 @@ class Cluster:
     is_admin = None
 
     def __new__(self, new_database_name: str, new_collection_name: str, new_is_admin: bool):
+        """
+        This method is automatically called when a Cluster object is created. It flexibly selects and returns the
+        singleton MongoDB instance depending on the database and collection names provided.
+
+        :param new_database_name: The name of the database to work with.
+        :param new_collection_name: The name of the collection to work with.
+        :param new_is_admin: A flag used to access the cluster as admin (True) or as a regular developer (False).
+        :return: The collection where documents can be accessed or stored.
+        """
         if (self.__database is None or self.database_name != new_database_name or
                 self.collection_name != new_collection_name or self.is_admin != new_is_admin):
             database_user_name = "dev1"
