3cc224ccfc20c6aa36dd9083a85565759a151955
Edward Chang <echang@unb.ca>
2021-10-06 22:54:36 -0300

    Initialize project (with example on pushing data to cluster)

---
+++ database.py
@@ -0,0 +1,9 @@
+import pymongo
+
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://dev1:dev1@cluster.yyiqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+database = cluster["database"]
+collection = database["test"]
+
+collection.insert_one({"_id": 0, "value": 0})
