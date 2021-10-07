41eb67323808931d424dd112541b0dd090025577
Edward Chang <echang@unb.ca>
2021-10-07 03:57:15 -0300

    Update test_cluster.py example data push value

--- backend/test_cluster.py
+++ backend/test_cluster.py
@@ -1,4 +1,4 @@
 from cluster import Cluster
 
 test_collection = Cluster("database", "test", False)
-test_collection.insert_one({"_id": 2, "value": 2})
+test_collection.insert_one({"_id": 3, "value": 3})
