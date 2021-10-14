ad4f17203f15760fe309af67a36e35614eb6746d
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2021-10-13 21:32:19 -0300

    Refactored scales.py

--- utils/scales.py
+++ utils/scales.py
@@ -1,67 +1,151 @@
-'''
-Author : Elliot
-Last Edit : Elliot (7.10.2021 12:04AM) 
-'''
+from music_tools import *
 
-# A major / minor notes (not for pentatonic scale)
-notes = ['C', 'C#', 'D', 'D#', 'E', 'F',  'F#', 'G', 'G#', 'A', 'A#', 'B']
+# notes = ['C', 'C#', 'D', 'D#', 'E', 'F',  'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-# A major / minor notes (for the pentatonic scales)
+# # Major Ionion Scale
+# I_major = 'WWHWWWH'
 
-# Major Ionion Scale
-I_major = 'WWHWWWH'
+# # Natural minor Aeolian Scale
+# A_minor = 'WHWWHWW'
 
-# Natural minor Aeolian Scale
-A_minor = 'WHWWHWW'
+# # Major Pentatonic Scale
+# P_major = 'WWPW'
 
-# Major Pentatonic Scale
-P_major = 'WWPW'
-
-# Minor Pentatonic Scale
-P_minor = 'PWWP'
+# # Minor Pentatonic Scale
+# P_minor = 'PWWP'
 
 
-def getScale(root, scaleType):
+def get_scale(root, type):
     '''
-    Returns either a major or minor chord with the root as the root note.
+    Returns a chord from the root node.
 
         Parameters:
-            root (String)        : the root note of the chord
-            scaleType (String)   : the type of chord to build
-
-        Returns:
-            the chord generated from the root note
+            root (String) : the root note of the chord
+            type (String) : the type of chord to build
+        Return:
+            a chord with the root as the chord type
     '''
-
-    # a list to hold the generated chord
+    # adds the root node into the list first
     scale = [root]
 
-    # The initial index of the note to start at
-    key = notes.index(root)
+    if type == 'M':
+        # WW
+        for _ in range(2):
+            root = whole_step(root)
+            scale.append(root)
 
-    if scaleType == 'M':
-        stepList = I_major
-    elif scaleType == 'm':
-        stepList = A_minor
-    elif scaleType == 'pM':
-        stepList = P_major
-    elif scaleType == 'pm':
-        stepList = P_minor
+        # H
+        root = half_step(root)
+        scale.append(root)
 
-    # if the scaleType selected is a major chord (M)
-    for char in stepList:
-        # Whole step += 2; half step += 1
-        if char == 'P':
-            key += 3
-        elif char == 'W':
-            key += 2
-        elif char == 'H':
-            key += 1
+        # WWW
+        for _ in range(3):
+            root = whole_step(root)
+            scale.append(root)
 
-        # to ensure that key does not go over the length of the notes array
-        key = key % len(notes)
+        # H
+        scale.append(half_step(root))
 
-        # add it to the list to return
-        scale.append(notes[key])
+        return scale
 
-    return scale
+    elif type == 'm':
+        # W
+        root = whole_step(root)
+        scale.append(root)
+
+        # H
+        root = half_step(root)
+        scale.append(root)
+
+        # WW
+        for _ in range(2):
+            root = whole_step(root)
+            scale.append(root)
+
+        # H
+        root = half_step(root)
+        scale.append(root)
+
+        # WW
+        for _ in range(2):
+            root = whole_step(root)
+            scale.append(root)
+
+        return scale
+
+    elif type == 'pM':
+        # WW
+        for _ in range(2):
+            root = whole_step(root)
+            scale.append(root)
+
+        # HW
+        root = whole_step(half_step(root))
+        scale.append(root)
+
+        # W
+        scale.append(whole_step(root))
+
+        return scale
+
+    elif type == 'pm':
+        # WH
+        root = whole_step(half_step(root))
+        scale.append(root)
+
+        # WW
+        for _ in range(2):
+            root = whole_step(root)
+            scale.append(root)
+
+        # WH
+        root = whole_step(half_step(root))
+        scale.append(root)
+
+        return scale
+
+# def getScale(root, scaleType):
+#     '''
+#     Returns either a major or minor chord with the root as the root note.
+
+#         Parameters:
+#             root (String)        : the root note of the chord
+#             scaleType (String)   : the type of chord to build
+
+#         Returns:
+#             the chord generated from the root note
+#     '''
+
+#     # a list to hold the generated chord
+#     scale = [root]
+
+#     # The initial index of the note to start at
+#     key = notes.index(root)
+
+#     if scaleType == 'M':
+#         stepList = I_major
+#     elif scaleType == 'm':
+#         stepList = A_minor
+#     elif scaleType == 'pM':
+#         stepList = P_major
+#     elif scaleType == 'pm':
+#         stepList = P_minor
+
+#     # if the scaleType selected is a major chord (M)
+#     for char in stepList:
+#         # Whole step += 2; half step += 1
+#         if char == 'P':
+#             key += 3
+#         elif char == 'W':
+#             key += 2
+#         elif char == 'H':
+#             key += 1
+
+#         # to ensure that key does not go over the length of the notes array
+#         key = key % len(notes)
+
+#         # add it to the list to return
+#         scale.append(notes[key])
+
+#     return scale
+
