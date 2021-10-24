0fb0def68d28de76c00007353b12c3286ef600b7
owenlee-dev <70532700+owenlee-dev@users.noreply.github.com>
2021-10-24 10:21:32 -0300

    Added mxml to abc capabilities

---
+++ backend/mxl_to_abc/mxl_to_abc_converter.py
@@ -0,0 +1,14 @@
+import os
+
+# Function to convert a single .mxl file into .abc
+# output file will be generated in converted_compositions dir
+def convert(input_file, output_path=os.getcwd()):
+  base_name=os.path.basename(input_file)
+  os.system("python xml2abc.py "+input_file+" -o converted_compositions "+ output_path+"/"+base_name)
+
+# Function that will convert all of the .mxl files in to_convert directory
+# output files will be generated in converted_compositions dir
+def batch_convert():
+  files_to_convert=os.listdir("to_convert") 
+  for file in files_to_convert:
+    convert("to_convert/"+file, "converted_compositions")
