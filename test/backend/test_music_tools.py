07834927f28d5005f88e0ae87a30028279b6e0d4
T-Camp <tcampbe6@unb.ca>
2021-11-08 17:36:58 -0400

    Commit to fix unit tests
    Fixing unit test format

--- test/backend/test_music_tools.py
+++ test/backend/test_music_tools.py
@@ -1,120 +1,123 @@
+import unittest
 from src.backend.music_tools import whole_step,half_step,M3,m3,P5,change_octave
 
-def test_halfStep_increasingFreq_one():
-  assert half_step("A", True)=="A#"
+class TextMusicTools(unittest.TestCase):
+
+  def test_halfStep_increasingFreq_one(self):
+    self.assertEqual(half_step("A", True), "A#")
   
-def test_halfStep_increasingFreq_two():
-  assert half_step("B", True)=="c'"
+  def test_halfStep_increasingFreq_two(self):
+    self.assertEqual(half_step("B", True), "c'")
 
-def test_halfStep_increasingFreq_three():
-  assert half_step("D#,", True)=="E,"
-
-
-def test_halfStep_decreasingFreq_one():
-  assert half_step("c'", False) == "B"
-
-def test_halfStep_decreasingFreq_two():
-  assert half_step("A", False) == "G#"
-
-def test_halfStep_decreasingFreq_three():
-  assert half_step("F,", False) == "E,"
+  def test_halfStep_increasingFreq_three(self):
+    self.assertEqual(half_step("D#,", True), "E,")
 
 
-def test_wholeStep_increasingFreq_one():
-  assert whole_step("E", True)=="F#"
+  def test_halfStep_decreasingFreq_one(self):
+    self.assertEqual(half_step("c'", False) ,  "B")
 
-def test_wholeStep_increasingFreq_two():
-  assert whole_step("B", True)=="c#'"
+  def test_halfStep_decreasingFreq_two(self):
+    self.assertEqual(half_step("A", False) ,  "G#")
 
-def test_wholeStep_increasingFreq_three():
-  assert whole_step("D,", True)=="E,"
-
-
-def test_wholeStep_decreasingFreq_one():
-  assert whole_step("c'", False)=="A#"
-
-def test_wholeStep_decreasingFreq_two():
-  assert whole_step("B", False)=="A"
-
-def test_wholeStep_decreasingFreq_three():
-  assert whole_step("F#,", False)=="E,"
+  def test_halfStep_decreasingFreq_three(self):
+    self.assertEqual(half_step("F,", False) ,  "E,")
 
 
-def test_M3_increasingFreq_one():
-  assert M3("C", True)=="E"
+  def test_wholeStep_increasingFreq_one(self):
+    self.assertEqual(whole_step("E", True), "F#")
 
-def test_M3_increasingFreq_two():
-  assert M3("A#", True)=="d'"
+  def test_wholeStep_increasingFreq_two(self):
+    self.assertEqual(whole_step("B", True), "c#'")
 
-def test_M3_increasingFreq_three():
-  assert M3("b'", True)=="d#''"
-
-
-def test_M3_decreasingFreq_one():
-  assert M3("c'", False)=="G#"
-
-def test_M3_decreasingFreq_two():
-  assert M3("E", False)=="C"
-
-def test_M3_decreasingFreq_three():
-  assert M3("F#,", False)=="D,"
+  def test_wholeStep_increasingFreq_three(self):
+    self.assertEqual(whole_step("D,", True), "E,")
 
 
-def test_m3_increasingFreq_one():
-  assert m3("E", True)=="G"
+  def test_wholeStep_decreasingFreq_one(self):
+    self.assertEqual(whole_step("c'", False), "A#")
 
-def test_m3_increasingFreq_two():
-  assert m3("c'", True)=="d#'"
+  def test_wholeStep_decreasingFreq_two(self):
+    self.assertEqual(whole_step("B", False), "A")
 
-def test_m3_increasingFreq_three():
-  assert m3("F,", True)=="G#,"
-
-
-def test_m3_decreasingFreq_one():
-  assert m3("C,", False)=="A,,"
-
-def test_m3_decreasingFreq_two():
-  assert m3("d''", False)=="b'"
-
-def test_m3_decreasingFreq_three():
-  assert m3("E", False)=="C#"
+  def test_wholeStep_decreasingFreq_three(self):
+    self.assertEqual(whole_step("F#,", False), "E,")
 
 
-def test_P5_increasingFreq_one():
-  assert P5("C", True)=="G"
+  def test_M3_increasingFreq_one(self):
+    self.assertEqual(M3("C", True), "E")
 
-def test_P5_increasingFreq_two():
-  assert P5("F#", True)=="c#'"
+  def test_M3_increasingFreq_two(self):
+    self.assertEqual(M3("A#", True), "d'")
 
-def test_P5_increasingFreq_three():
-  assert P5("A,", True)=="E"
-
-
-def test_P5_decreasingFreq_one():
-  assert P5("A#,", False)=="D#,"
-
-def test_P5_decreasingFreq_two():
-  assert P5("D", False)=="G,"
-
-def test_P5_decreasingFreq_three():
-  assert P5("e'", False)=="A"
+  def test_M3_increasingFreq_three(self):
+    self.assertEqual(M3("b'", True), "d#''")
 
 
-def test_changeOctave_increasingFreq_one():
-  assert change_octave("C", True)=="c'"
+  def test_M3_decreasingFreq_one(self):
+    self.assertEqual(M3("c'", False), "G#")
 
-def test_changeOctave_increasingFreq_two():
-  assert change_octave("g#'", True)=="g#''"
+def test_M3_decreasingFreq_two(self):
+  self.assertEqual(M3("E", False), "C")
 
-def test_changeOctave_increasingFreq_three():
-  assert change_octave("A,", True)=="A"
+def test_M3_decreasingFreq_three(self):
+  self.assertEqual(M3("F#,", False), "D,")
 
 
-def test_changeOctave_decreasingFreq_one():
-  assert change_octave("A#,", False)=="A#,,"
+def test_m3_increasingFreq_one(self):
+  self.assertEqual(m3("E", True), "G")
 
-def test_changeOctave_decreasingFreq_two():
-  assert change_octave("D", False)=="D,"
+def test_m3_increasingFreq_two(self):
+  self.assertEqual(m3("c'", True), "d#'")
 
-def test_changeOctave_decreasingFreq_three():
-  assert change_octave("e'", False)=="E"
+def test_m3_increasingFreq_three(self):
+  self.assertEqual(m3("F,", True), "G#,")
+
+
+def test_m3_decreasingFreq_one(self):
+  self.assertEqual(m3("C,", False), "A,,")
+
+def test_m3_decreasingFreq_two(self):
+  self.assertEqual(m3("d''", False), "b'")
+
+def test_m3_decreasingFreq_three(self):
+  self.assertEqual(m3("E", False), "C#")
+
+
+def test_P5_increasingFreq_one(self):
+  self.assertEqual(P5("C", True), "G")
+
+def test_P5_increasingFreq_two(self):
+  self.assertEqual(P5("F#", True), "c#'")
+
+def test_P5_increasingFreq_three(self):
+  self.assertEqual(P5("A,", True), "E")
+
+
+def test_P5_decreasingFreq_one(self):
+  self.assertEqual(P5("A#,", False), "D#,")
+
+def test_P5_decreasingFreq_two(self):
+  self.assertEqual(P5("D", False), "G,")
+
+def test_P5_decreasingFreq_three(self):
+  self.assertEqual(P5("e'", False), "A")
+
+
+def test_changeOctave_increasingFreq_one(self):
+  self.assertEqual(change_octave("C", True), "c'")
+
+def test_changeOctave_increasingFreq_two(self):
+  self.assertEqual(change_octave("g#'", True), "g#''")
+
+def test_changeOctave_increasingFreq_three(self):
+  self.assertEqual(change_octave("A,", True), "A")
+
+
+def test_changeOctave_decreasingFreq_one(self):
+  self.assertEqual(change_octave("A#,", False), "A#,,")
+
+def test_changeOctave_decreasingFreq_two(self):
+  self.assertEqual(change_octave("D", False), "D,")
+
+def test_changeOctave_decreasingFreq_three(self):
+  self.assertEqual(change_octave("e'", False), "E")
--- test/backend/test_scales.py
+++ test/backend/test_scales.py
@@ -1,37 +1,40 @@
-from src.backend.scales import getScale
+import unittest
+from src.backend.scales import get_scale
 
-def test_M_one():
-  assert getScale('C','M') == ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
+class TestScales(unittest.TestCase):
+
+  def test_M_one(self):
+    self.assertEqual(get_scale('C','M'), ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'])
   
-def test_M_two():
-  assert getScale('D#','M') == ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D', 'D#']
+  def test_M_two(self):
+    self.assertEqual(get_scale('D#','M'), ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D', 'D#'])
 
-def test_M_three():
-  assert getScale('F','M') == ['F', 'G', 'A', 'A#', 'C', 'D', 'E', 'F']
+  def test_M_three(self):
+    self.assertEqual(get_scale('F','M'), ['F', 'G', 'A', 'A#', 'C', 'D', 'E', 'F'])
 
-def test_m_one():
-  assert getScale('G#','m') == ['G#', 'A#', 'B', 'C#', 'D#', 'E', 'F#', 'G#']
+  def test_m_one(self):
+    self.assertEqual(get_scale('G#','m'), ['G#', 'A#', 'B', 'C#', 'D#', 'E', 'F#', 'G#'])
 
-def test_m_two():
-  assert getScale('B','m') == ['B', 'C#', 'D', 'E', 'F#', 'G', 'A', 'B']
+  def test_m_two(self):
+    self.assertEqual(get_scale('B','m'), ['B', 'C#', 'D', 'E', 'F#', 'G', 'A', 'B'])
 
-def test_m_three():
-  assert getScale('A#','m') == ['A#', 'C', 'C#', 'D#', 'F', 'F#', 'G#', 'A#']
+  def test_m_three(self):
+    self.assertEqual(get_scale('A#','m'), ['A#', 'C', 'C#', 'D#', 'F', 'F#', 'G#', 'A#'])
 
-def test_pM_one():
-  assert getScale('G','pM') == ['G', 'A', 'B', 'D', 'E']
+  def test_pM_one(self):
+    self.assertEqual(get_scale('G','pM'), ['G', 'A', 'B', 'D', 'E'])
 
-def test_pM_two():
-  assert getScale('C#','pM') == ['C#', 'D#', 'F', 'G#', 'A#']
+  def test_pM_two(self):
+    self.assertEqual(get_scale('C#','pM'), ['C#', 'D#', 'F', 'G#', 'A#'])
 
-def test_pM_three():
-  assert getScale('E','pM') == ['E', 'F#', 'G#', 'B', 'C#']
+  def test_pM_three(self):
+    self.assertEqual(get_scale('E','pM'), ['E', 'F#', 'G#', 'B', 'C#'])
 
-def test_pm_one():
-  assert getScale('G#','pm') == ['G#', 'B', 'C#', 'D#', 'F#']
+  def test_pm_one(self):
+    self.assertEqual(get_scale('G#','pm'), ['G#', 'B', 'C#', 'D#', 'F#'])
 
-def test_pm_two():
-  assert getScale('B','pm') == ['B', 'D', 'E', 'F#', 'A']
+  def test_pm_two(self):
+    self.assertEqual(get_scale('B','pm'), ['B', 'D', 'E', 'F#', 'A'])
 
-def test_pm_three():
-  assert getScale('A#','pm') == ['A#', 'C#', 'D#', 'F', 'G#']
+  def test_pm_three(self):
+    self.assertEqual(get_scale('A#','pm'), ['A#', 'C#', 'D#', 'F', 'G#'])
