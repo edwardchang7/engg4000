2ef13031cabed38a123c2e9b6cd775c9c07b4572
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2021-11-17 19:29:59 -0400

    Added function to flip the notation and added function to count the number of repeating patterns

--- src/backend/tonal_patterns.py
+++ src/backend/tonal_patterns.py
@@ -1,75 +1,109 @@
-from music_tools import *
-from scales import *
+from src.backend.music_tools import *
+from src.backend.scales import *
+import itertools
 
-def analyze_patterns(key = 'GM', input_string = 'E/ | G A/ (B3/4 c/4) B/ | A F/ (D3/4 E/4) F/ | G E/ (E3/4 ^D/4) E/ | F3/ B, E/ | G A/ (B3/4 c/4) B/ | A F/ (D3/4 E/4) F/ | (G3/4 F/4) E/ (^D3/4 ^C/4) D/ | E3/ E3/ |"^Chorus" d3/ (d3/4 c/4) B/ | A F/ (D3/4 E/4) F/ | G E/ (E3/4 ^D/4) E/ | F ^D/ B,3/ | d3/ (d3/4 c/4) B/ | A F/ (D3/4 E/4) F/ | (G3/4 F/4) E/ (^D3/4 ^C/4) D/ | E3/ E|]'):
+def analyze_patterns(key = 'GM', input_string = "E'/ | G A/ (B3/4 c/4) B/"):
+
+    # list of symbols to keep track of
+    exceptions = ["'", ",", "_", "^", "|"]
 
     analyze_str = ""
     flag = True
 
     for char in input_string:
-        if char == '"': #removes "chorus" from string
+        if char == '"': #removes any strings within ""
             flag = not flag
+        
         if flag:
-            if char.isalpha() or char == "'" or char == "," or char == "_" or char == "^" or char == "|": #removes rythmyic related things
+            if char.isalpha() or char in exceptions: #removes rythmyic related things
                 analyze_str += char
-
-    print(get_scale(key[0], key[1]))
-    print(analyze_str)
 
     #hardcoded for now to test pattern freq
     pattern = ['h', '-h']
 
     print(frequency_of_pattern(analyze_str, key, pattern))
-    print(frequency_of_pattern(analyze_str, key, pattern[::-1]))
 
-def frequency_of_pattern(analyze_str, key, pattern):
+def frequency_of_pattern(analyze_str,key, pattern):
 
     prev_chars = ""
     prev_notes = []
     total = 0
+    is_flat = False
 
     for char in analyze_str:
-        print(char)
-        print(prev_chars)
-        print(prev_notes)
         if char == "'" or char == ",":
             prev_chars += char
         elif char != "|" and char != " ":
             if prev_chars[:1] == "^":
-                prev_chars += "#"
+                prev_chars = "#"
             elif prev_chars[:1] == "_": #fix to work with flats
-                prev_chars += "_"
+                prev_chars = "#"
+                is_flat = True
             else:
                 if prev_chars != "":
-                    prev_notes.append(prev_chars)
+                    prev_notes.append(flip(prev_chars))
+
                 prev_chars = ""
-                prev_chars += char
 
-        if len(prev_notes) == len(pattern) + 1:
-            i = 0
-            up = True
-            test_note = ""
-            for note in prev_notes:
+            if is_flat is True: #if it is a flat, set it to the sharp equivalent
+                char = whole_step(char, False)
+                is_flat = False
 
-                if test_note != "":
-                    if test_note != note:
-                        break
-                if i < len(pattern):
-                    if pattern[i][0] == '-':
-                        up = False
-                        pattern[i][0] = pattern[i][1]
+            prev_chars += char
 
-                    if pattern[i][0] == 'h':
-                        print(note)
-                        test_note = half_step(note, up)
+    prev_notes.append(flip(prev_chars))
 
-                i + 1
+# prev_notes = ["A", "#A", "A"]
+# pattern = [h, hh]
 
-            if i == len(prev_notes):
+    total = 0
 
-                total += 1
+    print(prev_notes)
 
-            prev_chars = ""
-            prev_notes = []
+    for i in range (1, len(prev_notes)): #inclusive start, exclusive end
+        if is_half_step(prev_notes[i], prev_notes[i-1]):
+            total += 1
+
+    
+
+
+    # if len(prev_notes) == len(pattern) + 1:
+    #     i = 0
+    #     up = True
+    #     test_note = ""
+    #     for note in prev_notes:
+    #         if test_note != "":
+    #             if test_note != note:
+    #                 break
+    #         if i < len(pattern):
+    #             if pattern[i][0] == '-':
+    #                 up = False
+    #                 pattern[i] = 'h'
+
+    #             if pattern[i][0] == 'h':
+    #                 test_note = half_step(note, up)
+                
+    #         if test_note == "A#":
+    #             test_note = "#A"
+
+    #         i += 1
+
+    #     if i == len(prev_notes):
+    #         total += 1
+
+    #     prev_chars = ""
+    #     prev_notes = []
 
     return total
+
+
+def flip(note):
+    '''
+    A function to 'fix' the notation. If the note given is '#A', it will return 'A#'
+
+    Param:
+        the note to flip
+    '''
+    if len(note) == 2:
+        return note[1] + note[0]
+    return note
