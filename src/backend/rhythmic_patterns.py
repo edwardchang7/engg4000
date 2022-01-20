6e1898b3b48de24b94c502dd2ee01351d5361654
owenlee-dev <owenlee2@hotmail.com>
2022-01-19 20:34:35 -0400

    Added initial rhythmic pattern encoding

--- src/backend/rhythmic_patterns.py
+++ src/backend/rhythmic_patterns.py
@@ -1,17 +1,55 @@
 """
 This file will be filled with the functions required to extract and save 
 rhythmic patterns from an abc file
-"""
 
+1. If polyphonic - seperate
+2. Get meter and time signature
+3. Translate notes in bars into numbers representing note lengths
+4. Analyze numbers looking for patterns
+5. Cross reference those sections with original music to get actual musical pattern
+"""
+import re
+from typing import List
 from abc_tools import get_header,is_polyphonic,get_voicings,get_music
 
+def extract_rhythmic_patterns(file_path:str):
+  voicings=get_voicings(file_path)
+  meter=get_header(file_path,'M')
+  encoded_voicings=encode_voicings(voicings)
+  
 
-# Testing for the abc_tools
-voicings=get_voicings('mxl_to_abc\converted_compositions\Cant_help_falling_in_love__Elvis_Presley.abc')
+# Function to isolate the notes in a single bar
+def format_bar(bar:str):
+  has_notes=re.search('[A-Ga-g]',bar)
+  if not has_notes:
+    bar=None
+  else:
+    bar=re.sub('%[0-9][0-9]?[0-9]?',"",bar)
+    bar=re.sub('"[^"]*"',"",bar)
+    bar=re.sub('![^"]*!',"",bar)
+    bar=re.sub('[[A-Z]:[^"]*]',"",bar)
+    bar=bar.replace("\n","")
+    bar=bar.replace("$","")
+    bar=bar.strip()
+  return bar
 
-for voice in voicings:
-  print(voice)
-  print("\n\n")
+# Function to encode a musical bar into numbers representing the note lengths
+# def encode_bar(bar:str):
 
-print(len(voicings))
+def encode_voicings(voicings):
+  #1. isolate notes
+    # split into sections
+    # split sections into bars
+  for voice in voicings:
+    sections=voice.split('||')
+    for section in sections:
+      bars=section.split("|")
+      for bar in bars:
+        bar=format_bar(bar)
+        print("--> "+bar)
+        # if(bar):
+        #   bar=encode_bar(bar)
+        
+  #2. Change notes in bars into a number
 
+extract_rhythmic_patterns('mxl_to_abc/converted_compositions/Dancing_in_the_Moonlight.abc')
