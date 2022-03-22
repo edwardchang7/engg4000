cb82172aa729795ba3263b8726cb96826cc72f92
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-22 13:51:19 -0300

    Added comments

--- src/backend/Collections/Song_Collection.py
+++ src/backend/Collections/Song_Collection.py
@@ -4,23 +4,22 @@ A collection to hold a list of patterns within the song
 
 class Song_Collection:
 
+    '''
+    Constructor
+    '''
     def __init__(self, song_name, v1_patterns=None, v2_patterns=None):
         self.song_name = song_name
 
         # if a pattern list is passed in then use that pattern list, else create an empty list
-        if v1_patterns:
-            self.v1_patterns = v1_patterns
-        else:
-            self.v1_patterns = []
-
-        if v2_patterns:
-            self.v2_patterns = v2_patterns
-        else:
-            self.v2_patterns = []
+        self.v1_patterns = v1_patterns if v1_patterns else []
+        self.v2_patterns = v2_patterns if v2_patterns else []
 
 
     '''
-    adds a given pattern to either v1 / v2
+    Adds a pattern to either v1 or v2 patterns
+
+    Parameters
+        pattern : the pattern to add to v1 or v2
     '''
     def add_pattern(self, pattern) -> None:
         if pattern.frequency > 1:
@@ -31,14 +30,14 @@ class Song_Collection:
 
 
     '''
-    returns both v1 and v2
+    Returns both v1 and v2 patterns
     '''
-    def get_patterns(self):
+    def get_patterns(self) -> list:
         return self.v1_patterns, self.v2_patterns
 
 
     '''
-    toString method
+    ToString function
     '''
     def __str__(self):
         to_return = self.song_name + "\n"
