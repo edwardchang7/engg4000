54ee994f766b7462df43d3dc72d96327a7c645cd
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-25 16:38:58 -0300

    Update SongBuilder.py
    Added Edward's functions here

--- src/backend/Song_Builder/SongBuilder.py
+++ src/backend/Song_Builder/SongBuilder.py
@@ -173,3 +173,21 @@ def _make_db_connection(database_name, is_admin, collection_name=None):
         database = Cluster(database_name, "", is_admin)
 
     return database
+
+def build_new_pattern(self, first_existing_pattern: list, second_existing_pattern: list, desired_num_of_beats: int) -> list:
+        num_of_beats_in_first_pattern = len(first_existing_pattern)
+        num_of_beats_in_second_pattern = len(second_existing_pattern)
+
+        num_of_beats_in_new_pattern = desired_num_of_beats - num_of_beats_in_first_pattern - num_of_beats_in_second_pattern
+
+        if num_of_beats_in_new_pattern < 0:
+            return None
+        elif num_of_beats_in_new_pattern == 0:
+            return combine_patterns(first_existing_pattern, second_existing_pattern)
+        else:
+            pass
+            # find common scale
+            # return new pattern
+        
+def combine_patterns(self, first_pattern: list, second_pattern: list) -> list:
+    return first_pattern + second_pattern
