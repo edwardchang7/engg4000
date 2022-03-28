5de173b45b1b6ccd186a9610e47f987c8d9b8986
T-Camp <tcampbe6@unb.ca>
2022-03-28 09:59:05 -0300

    Music tools tests fixes
    Fixes the tests to go along with the hafl_step fix

--- test/backend/test_music_tools.py
+++ test/backend/test_music_tools.py
@@ -6,67 +6,67 @@ from src.backend.music_tools import whole_step, half_step, M3, m3, P5, change_oc
 class TextMusicTools(unittest.TestCase):
 
     def test_halfStep_increasingFreq_one(self):
-        self.assertEqual(half_step("A", True), "#A")
+        self.assertEqual(half_step("A", True), "A#")
 
     def test_halfStep_increasingFreq_two(self):
         self.assertEqual(half_step("B", True), "c")
 
     def test_halfStep_increasingFreq_three(self):
-        self.assertEqual(half_step("#D,", True), "E,")
+        self.assertEqual(half_step("D#,", True), "E,")
 
     def test_halfStep_decreasingFreq_one(self):
         self.assertEqual(half_step("c'", False), "b")
 
     def test_halfStep_decreasingFreq_two(self):
-        self.assertEqual(half_step("A", False), "#G")
+        self.assertEqual(half_step("A", False), "G#")
 
     def test_halfStep_decreasingFreq_three(self):
         self.assertEqual(half_step("F,", False), "E,")
 
     def test_wholeStep_increasingFreq_one(self):
-        self.assertEqual(whole_step("E", True), "#F")
+        self.assertEqual(whole_step("E", True), "F#")
 
     def test_wholeStep_increasingFreq_two(self):
-        self.assertEqual(whole_step("B", True), "#c")
+        self.assertEqual(whole_step("B", True), "c#")
 
     def test_wholeStep_increasingFreq_three(self):
         self.assertEqual(whole_step("D,", True), "E,")
 
     def test_wholeStep_decreasingFreq_one(self):
-        self.assertEqual(whole_step("c'", False), "#a")
+        self.assertEqual(whole_step("c'", False), "a#")
 
     def test_wholeStep_decreasingFreq_two(self):
         self.assertEqual(whole_step("B", False), "A")
 
     def test_wholeStep_decreasingFreq_three(self):
-        self.assertEqual(whole_step("#F,", False), "E,")
+        self.assertEqual(whole_step("F#,", False), "E,")
 
     def test_M3_increasingFreq_one(self):
         self.assertEqual(M3("C", True), "E")
 
     def test_M3_increasingFreq_two(self):
-        self.assertEqual(M3("#A", True), "d")
+        self.assertEqual(M3("A#", True), "d")
 
     def test_M3_increasingFreq_three(self):
-        self.assertEqual(M3("b'", True), "#d''")
+        self.assertEqual(M3("b'", True), "d#''")
 
     def test_M3_decreasingFreq_one(self):
-        self.assertEqual(M3("c'", False), "#g")
+        self.assertEqual(M3("c'", False), "g#")
 
     def test_M3_decreasingFreq_two(self):
         self.assertEqual(M3("E", False), "C")
 
     def test_M3_decreasingFreq_three(self):
-        self.assertEqual(M3("#F,", False), "D,")
+        self.assertEqual(M3("F#,", False), "D,")
 
     def test_m3_increasingFreq_one(self):
         self.assertEqual(m3("E", True), "G")
 
     def test_m3_increasingFreq_two(self):
-        self.assertEqual(m3("c'", True), "#d'")
+        self.assertEqual(m3("c'", True), "d#'")
 
     def test_m3_increasingFreq_three(self):
-        self.assertEqual(m3("F,", True), "#G,")
+        self.assertEqual(m3("F,", True), "G#,")
 
     def test_m3_decreasingFreq_one(self):
         self.assertEqual(m3("C,", False), "A,,")
@@ -75,19 +75,19 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(m3("d''", False), "b'")
 
     def test_m3_decreasingFreq_three(self):
-        self.assertEqual(m3("E", False), "#C")
+        self.assertEqual(m3("E", False), "C#")
 
     def test_P5_increasingFreq_one(self):
         self.assertEqual(P5("C", True), "G")
 
     def test_P5_increasingFreq_two(self):
-        self.assertEqual(P5("#F", True), "#c")
+        self.assertEqual(P5("F#", True), "c#")
 
     def test_P5_increasingFreq_three(self):
         self.assertEqual(P5("A,", True), "E")
 
     def test_P5_decreasingFreq_one(self):
-        self.assertEqual(P5("#A,", False), "#D,")
+        self.assertEqual(P5("A#,", False), "D#,")
 
     def test_P5_decreasingFreq_two(self):
         self.assertEqual(P5("D", False), "G,")
@@ -99,13 +99,13 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(change_octave("C", True), "c")
 
     def test_changeOctave_increasingFreq_two(self):
-        self.assertEqual(change_octave("#g'", True), "#g''")
+        self.assertEqual(change_octave("g#'", True), "g#''")
 
     def test_changeOctave_increasingFreq_three(self):
         self.assertEqual(change_octave("A,", True), "A")
 
     def test_changeOctave_decreasingFreq_one(self):
-        self.assertEqual(change_octave("#A,", False), "#A,,")
+        self.assertEqual(change_octave("A#,", False), "A#,,")
 
     def test_changeOctave_decreasingFreq_two(self):
         self.assertEqual(change_octave("D", False), "D,")
