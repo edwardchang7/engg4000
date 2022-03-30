bcf6ba63b700eeea09ee9ae6640c7f667a180390
T-Camp <tcampbe6@unb.ca>
2022-03-30 19:03:14 -0300

    Finalizing tonal patterns into DB
    Fixes in tonal patterns to allow the function to properly analyze more songs. Database functions added to add patterns into the database with cluster.

--- src/backend/collections/tonal_pattern.py
+++ src/backend/collections/tonal_pattern.py
@@ -3,15 +3,10 @@ A class to hold a tonal pattern
 '''
 
 class TonalPattern:
-
-    def __init__(self, pattern, num_notes, priority):
-        # converts a string in the format of a list to an actual list object
+    def __init__(self, pattern: list, num_of_notes: int, priority: int):
         self.pattern = pattern
         self.priority = priority
-        self.num_notes = num_notes
+        self.num_of_notes = num_of_notes
 
-    '''
-    toString method
-    '''
     def __str__(self):
-        return (f"Pattern: {self.pattern} \nnum_notes: {self.num_notes} \nPriority: {self.priority} \n")
+        return f"pattern: {self.pattern}\nnum_notes: {self.num_of_notes}\npriority: {self.priority}\n"
