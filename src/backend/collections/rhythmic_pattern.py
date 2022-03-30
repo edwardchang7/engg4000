fbde133f47d5a1847166bb5ed1e9511609b0281d
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-30 20:01:00 -0300

    Changed the way beats are calculated

--- src/backend/collections/rhythmic_pattern.py
+++ src/backend/collections/rhythmic_pattern.py
@@ -1,6 +1,7 @@
 import ast
 
 
+
 class Rhythmic_Pattern:
     def __init__(self, pattern, frequency, is_v1):
         # Converts a string in the format of a list to an actual list object
@@ -9,11 +10,11 @@ class Rhythmic_Pattern:
 
         if type(self.pattern) is list:
             self.length = len(self.pattern)
-            self.beats = _get_beats_length(str(pattern))
+            self.beats = _get_length_in_beats(str(pattern))
         else:
             self.pattern = ast.literal_eval(self.pattern)
             self.length = len(self.pattern)
-            self.beats = _get_beats_length(pattern)
+            self.beats = _get_length_in_beats(pattern)
 
         self.is_v1 = is_v1
 
@@ -21,20 +22,34 @@ class Rhythmic_Pattern:
         return f"Pattern: {self.pattern} \nFrequency: {self.frequency} \nLength: {self.length} \nBeats: {self.beats}\nIs V1: {self.is_v1}\n"
 
 
-def _get_beats_length(pattern):
+def _get_length_in_beats(pattern):
     length = 0
     to_count = True
+    is_chord = False
+    last_char = ''
 
     # counts the length of each bar within the combined pattern
     for char in pattern:
         if char == '(':
             to_count = False
 
+        if char == "'":
+            last_char = char
+
+        if last_char == "'" and char == '[':
+            is_chord = True
+            length += 1
+
+        if last_char == "'" and char == ']':
+            is_chord = False
+            last_char = ""
+
         # if its a digit, then check if to_count is true (to_count will be false if its a rest's beat)
-        if char.isdigit() and to_count:
+        if char.isdigit() and to_count and not is_chord:
             length += 1
 
         elif char.isdigit() and not to_count:
             to_count = True
 
     return length
+
