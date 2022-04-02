969e00434c36b4ad8ca1af1f276da48e12bc3ea1
edwardchang7 <43451217+edwardchang7@users.noreply.github.com>
2022-04-02 13:07:03 -0300

    Revert "Revert "Tonal patterns"" (#54)

--- test/backend/test_music_tools.py
+++ test/backend/test_music_tools.py
@@ -1,5 +1,6 @@
 import unittest
-from src.backend.music_tools import whole_step, half_step, M3, m3, P5, change_octave
+from src.backend.music_tools import whole_step, half_step, M3, m3, P5, change_octave, pad_octave_notation, \
+    check_interval
 
 
 class TextMusicTools(unittest.TestCase):
@@ -8,13 +9,13 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(half_step("A", True), "A#")
 
     def test_halfStep_increasingFreq_two(self):
-        self.assertEqual(half_step("B", True), "c'")
+        self.assertEqual(half_step("B", True), "c")
 
     def test_halfStep_increasingFreq_three(self):
         self.assertEqual(half_step("D#,", True), "E,")
 
     def test_halfStep_decreasingFreq_one(self):
-        self.assertEqual(half_step("c'", False), "B")
+        self.assertEqual(half_step("c'", False), "b")
 
     def test_halfStep_decreasingFreq_two(self):
         self.assertEqual(half_step("A", False), "G#")
@@ -26,13 +27,13 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(whole_step("E", True), "F#")
 
     def test_wholeStep_increasingFreq_two(self):
-        self.assertEqual(whole_step("B", True), "c#'")
+        self.assertEqual(whole_step("B", True), "c#")
 
     def test_wholeStep_increasingFreq_three(self):
         self.assertEqual(whole_step("D,", True), "E,")
 
     def test_wholeStep_decreasingFreq_one(self):
-        self.assertEqual(whole_step("c'", False), "A#")
+        self.assertEqual(whole_step("c'", False), "a#")
 
     def test_wholeStep_decreasingFreq_two(self):
         self.assertEqual(whole_step("B", False), "A")
@@ -44,13 +45,13 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(M3("C", True), "E")
 
     def test_M3_increasingFreq_two(self):
-        self.assertEqual(M3("A#", True), "d'")
+        self.assertEqual(M3("A#", True), "d")
 
     def test_M3_increasingFreq_three(self):
         self.assertEqual(M3("b'", True), "d#''")
 
     def test_M3_decreasingFreq_one(self):
-        self.assertEqual(M3("c'", False), "G#")
+        self.assertEqual(M3("c'", False), "g#")
 
     def test_M3_decreasingFreq_two(self):
         self.assertEqual(M3("E", False), "C")
@@ -80,7 +81,7 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(P5("C", True), "G")
 
     def test_P5_increasingFreq_two(self):
-        self.assertEqual(P5("F#", True), "c#'")
+        self.assertEqual(P5("F#", True), "c#")
 
     def test_P5_increasingFreq_three(self):
         self.assertEqual(P5("A,", True), "E")
@@ -92,10 +93,10 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(P5("D", False), "G,")
 
     def test_P5_decreasingFreq_three(self):
-        self.assertEqual(P5("e'", False), "A")
+        self.assertEqual(P5("e'", False), "a")
 
     def test_changeOctave_increasingFreq_one(self):
-        self.assertEqual(change_octave("C", True), "c'")
+        self.assertEqual(change_octave("C", True), "c")
 
     def test_changeOctave_increasingFreq_two(self):
         self.assertEqual(change_octave("g#'", True), "g#''")
@@ -110,6 +111,39 @@ class TextMusicTools(unittest.TestCase):
         self.assertEqual(change_octave("D", False), "D,")
 
     def test_changeOctave_decreasingFreq_three(self):
-        self.assertEqual(change_octave("e'", False), "E")
+        self.assertEqual(change_octave("e'", False), "e")
 
+    def test_pad_octave_notation_octave0(self):
+        self.assertEqual(pad_octave_notation(0, "A"), "A")
 
+    def test_pad_octave_notation_octave1(self):
+        self.assertEqual(pad_octave_notation(1, "A"), "a")
+
+    def test_pad_octave_notation_octaveN1(self):
+        self.assertEqual(pad_octave_notation(-1, "A"), "A,")
+
+    def test_pad_octave_notation_octave2(self):
+        self.assertEqual(pad_octave_notation(2, "A"), "a'")
+
+    def test_pad_octave_notation_octaveN2(self):
+        self.assertEqual(pad_octave_notation(-2, "A"), "A,,")
+
+    def test_check_interval_0(self):
+        self.assertEqual(
+            check_interval({"note": "D", "degree": "0", "octave": 0}, {"note": "D", "degree": "0", "octave": 0}), ["0"])
+
+    def test_check_interval_1(self):
+        self.assertEqual(
+            check_interval({"note": "D", "degree": "0", "octave": 0}, {"note": "D", "degree": "0", "octave": 0}), ["0"])
+
+    def test_check_interval_2(self):
+        self.assertEqual(
+            check_interval({"note": "D", "degree": "0", "octave": 0}, {"note": "D", "degree": "0", "octave": 0}), ["0"])
+
+    def test_check_interval_3(self):
+        self.assertEqual(
+            check_interval({"note": "D", "degree": "0", "octave": 0}, {"note": "D", "degree": "0", "octave": 0}), ["0"])
+
+    def test_check_interval_4(self):
+        self.assertEqual(
+            check_interval({"note": "D", "degree": "0", "octave": 0}, {"note": "D", "degree": "0", "octave": 0}), ["0"])
