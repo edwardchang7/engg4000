3bd10888f2dc85bad90e7a02302d3ff2fc50ce33
Elliot-Chin <ychin@unb.ca>
2022-04-05 17:18:56 -0300

    Update note_pattern.py

--- src/backend/collections/note_pattern.py
+++ src/backend/collections/note_pattern.py
@@ -3,6 +3,9 @@ A class to hold a note and the length
 '''
 
 class Note_Pattern:
+
+    # save memory space by defining the attributed beforehand
+    __slots__ = ('note', 'length')
     
     def __init__(self, note:str, length:int):
         self.note = note
