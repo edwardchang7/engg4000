d01f993c4ebc6e37bb16f9a5fbaa4ab8dfac68f5
Elliot-Chin <ychin@unb.ca>
2022-04-10 01:20:19 -0300

    Update MessageDialog.java
    Changed the size of the messageDialog

--- src/frontend/Swing_UI/MessageDialog.java
+++ src/frontend/Swing_UI/MessageDialog.java
@@ -26,7 +26,7 @@ public class MessageDialog extends JDialog {
 	public MessageDialog(String title, String context, ImageIcon icon) {
 		setModal(true);
 		setModalityType(ModalityType.APPLICATION_MODAL);
-		setBounds(100, 100, 450, 193);
+		setBounds(100, 100, 400, 200);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setLayout(null);
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -43,14 +43,14 @@ public class MessageDialog extends JDialog {
 		 * ButtonPanel and buttons
 		 */
 		buttonPanel = new JPanel();
-		buttonPanel.setBounds(10, 132, 430, 50);
+		buttonPanel.setBounds(10, 132, 380, 50);
 
 		contentPanel.add(buttonPanel);
 
 		contextLabel = new JLabel(HEADER + context + FOOTER);
 		contextLabel.setFont(StandardFont.getInstance());
 		contextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contextLabel.setBounds(10, 46, 430, 75);
+		contextLabel.setBounds(10, 46, 380, 75);
 		contentPanel.add(contextLabel);
 
 		ok = new CustomButton("Ok");
